a83cdc568932565cf076f50a2c53e98ea3b2b562
Nicholas Firth-McCoy <nicholas@2suggestions.com.au>
2015-05-06 13:12:00 +1000

    Install neovim python package and prerequisites

--- bootstrap.sh
+++ bootstrap.sh
@@ -36,12 +36,15 @@ NOKOGIRI="libxml2-dev libxslt1-dev"
 POSTGRESQL="postgresql-9.3 postgresql-contrib-9.3 libpq-dev"
 YOUCOMPLETEME="cmake python-dev"
 DEJA_DUP_S3_STORAGE="python-boto dconf-editor"
-NEOVIM="neovim xclip"
+NEOVIM="neovim xclip python-dev python-pip python3-dev python3-pip"
 sudo apt-get update -q=2
 sudo apt-get install -q=2 -y --force-yes build-essential zlib1g-dev libssl-dev libreadline-dev curl git-core vim zsh firefox-trunk\
   heroku-toolbelt redis-server gnome-shell gnome-session htop memcached dropbox google-chrome-beta tmux libjemalloc1\
   password-gorilla msttcorefonts imagemagick colordiff lxc-docker libsqlite3-dev exuberant-ctags\
   ${THESILVERSEARCHER} ${NOKOGIRI} ${POSTGRESQL} ${YOUCOMPLETEME} ${DEJA_DUP_S3_STORAGE} ${NEOVIM}
+
+# Install neovim python package
+pip install --user neovim
 
 # Configure postgresql
 sudo sh -c "echo 'local all all trust' > /etc/postgresql/9.3/main/pg_hba.conf"
