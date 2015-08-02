b458b0a57f2da4b1ec8981c31cbb17c7c1b4a6e4
Nicholas Firth-McCoy <nicholas@2suggestions.com.au>
2015-08-02 11:14:36 +1000

    Install inotify-tools
    Phoenix needs this for efficient filesystem watching. I'm not sure how
    other tools (eg. guard) are interfacing with inotify, they don't seem to
    need this package to function efficiently.

--- bootstrap.sh
+++ bootstrap.sh
@@ -37,11 +37,12 @@ POSTGRESQL="postgresql-9.3 postgresql-contrib-9.3 libpq-dev"
 YOUCOMPLETEME="cmake python-dev"
 DEJA_DUP_S3_STORAGE="python-boto python-cloudfiles dconf-editor"
 NEOVIM="neovim xclip python-dev python-pip python3-dev python3-pip"
+PHOENIX="inotify-tools"
 sudo apt-get update -q=2
 sudo apt-get install -q=2 -y --force-yes build-essential zlib1g-dev libssl-dev libreadline-dev curl git-core vim zsh firefox-trunk\
   heroku-toolbelt redis-server gnome-shell gnome-session htop memcached dropbox google-chrome-beta tmux libjemalloc1\
   password-gorilla msttcorefonts imagemagick colordiff lxc-docker libsqlite3-dev exuberant-ctags flashplugin-installer\
-  ${THESILVERSEARCHER} ${NOKOGIRI} ${POSTGRESQL} ${YOUCOMPLETEME} ${DEJA_DUP_S3_STORAGE} ${NEOVIM}
+  ${THESILVERSEARCHER} ${NOKOGIRI} ${POSTGRESQL} ${YOUCOMPLETEME} ${DEJA_DUP_S3_STORAGE} ${NEOVIM} ${PHOENIX}
 
 # Install neovim python package
 pip install --user neovim
