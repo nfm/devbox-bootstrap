3eb6e2d7957176d4456e887e4791e15771f63eaf
Nicholas Firth-McCoy <nicholas@2suggestions.com.au>
2018-12-29 10:42:49 +1100

    Remove mention of hardcoding DNS servers
    ISPs usually proxy these, and hardcoding them means that my VPN can't override
    them dynamically, which enables sites to geoblock using DNS records.
    
    Just use the ISP dynamically provided DNS servers by default to avoid this.

--- bootstrap.sh
+++ bootstrap.sh
@@ -108,7 +108,6 @@ echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo s
 
 echo "Done. You'll need to:"
 echo "* Log out and back in to change your shell and desktop environment"
-echo "* Set DNS servers to 8.8.4.4 and 8.8.8.8"
 echo "* Manually copy in SSH keys and config"
 echo "* Manually copy in GPG keys"
 echo "* Set up backups (http://blog.domenech.org/2013/01/backing-up-ubuntu-using-deja-dup-backup-and-aws-s3.html) (ignore downloads, tmp, .rubies, .gem, .berkshelf, .cache, .npm, .nvm, .vim/bundle, .heroku, possibly others!)"
