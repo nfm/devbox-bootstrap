a785b7a7b5d93f0d9940402bb61d207172900d3f
Nicholas Firth-McCoy <nicholas@2suggestions.com.au>
2019-01-04 08:08:54 +1100

    Fix Google Chrome PPA
    Previously this caused warnings as Chrome doesn't publish packages for
    i386, and `dpkg --print-foreign-architectures` expects to be able to
    install these packages.

--- bootstrap.sh
+++ bootstrap.sh
@@ -24,7 +24,7 @@ sudo apt update && sudo apt install -y wget curl apt-transport-https ca-certific
 sudo add-apt-repository -y ppa:ubuntu-mozilla-daily/ppa &> /dev/null
 
 # - Chrome
-sudo sh -c "echo 'deb http://dl.google.com/linux/chrome/deb/ stable main' > /etc/apt/sources.list.d/google-chrome.list"
+sudo sh -c "echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' > /etc/apt/sources.list.d/google-chrome-beta.list"
 wget --quiet -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 
 # - Heroku toolbelt
