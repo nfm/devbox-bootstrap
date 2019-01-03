93a869f7cef33b780de0e42cc949e12eb31af4d3
Nicholas Firth-McCoy <nicholas@2suggestions.com.au>
2019-01-04 09:07:40 +1100

    Allow docker to be run as non-root

--- bootstrap.sh
+++ bootstrap.sh
@@ -90,6 +90,9 @@ sudo sh -c "echo 'local all all trust\nhost all all 127.0.0.1/32 trust' > /etc/p
 sudo -u postgres psql -c "DROP ROLE IF EXISTS `whoami`; CREATE ROLE `whoami` SUPERUSER LOGIN;"
 sudo service postgresql restart
 
+# Allow docker to be run as non-root
+sudo usermod -aG docker $USER
+
 # Configure deja-dup backup location
 gsettings set org.gnome.DejaDup.S3 bucket 'nfm-backups'
 
